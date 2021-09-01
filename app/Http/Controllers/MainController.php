b6bed7a7b3f4b5ec51e905ceeae9cf18b94fa0fa
szyuban <s.zyuban@it.ecommpay.com>
2021-09-01 10:56:31 +0300

    Big data file read

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,10 +10,39 @@ class MainController extends Controller
 {
     public function home() 
     {
-        $xml = Storage::get('public/import0_1.xml');
-        $xmlArray = (new XmlService())->XmlToArray($xml);
+        $xmlFiles = Storage::files('public');
+        foreach($xmlFiles as $file) {
+
+            $xml = Storage::get($file);
+            $xmlArray = (new XmlService())->XmlToArray($xml);
+        }
         dd($xmlArray);
+        
 
         return view('home', );
     }
 }
+
+// public function read_file($file)
+// {
+//     $fp = fopen($file, 'r');
+
+//     while(($line = fgets($fp)) !== false)
+//         yield $line;
+
+//     fclose($fp);
+// }  
+
+// public function upload(Request $request){
+//             set_time_limit(0);
+//             ini_set('MAX_EXECUTION_TIME', 36000);
+//             $file = Input::file('file');
+//             $filePath = $file->getRealPath();
+
+//             foreach(read_file($filePath) as $line)
+//             {
+//                << DO THE DATABASE OPERATION >>
+//             }
+
+//       return redirect()->back()->with('success', 'File uploaded successfully');
+//     }
