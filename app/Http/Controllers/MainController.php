34d9bf448fbe9b28c8fb349eb18e6c87fc8f1c83
zubans <zubans@mail.ru>
2021-10-04 17:22:31 +0300

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,26 +10,8 @@ class MainController extends Controller
 {
     public function home()
     {
-        (new WorkWithBigFilesService())->addFileOfGoodsToDataBase();
+        return (new WorkWithBigFilesService())->addFileOfGoodsToDataBase();
 
-//        $xml = Storage::get('public/import0_1.xml');
-//        $file = (new WorkWithBigFilesService())->readBigFile();
-//        $xmlArray = (new XmlService())->XmlToArray($file);
-//        dd($xmlArray);
-//        $xmlFiles = Storage::get(App::basePath(). '/storage/public/import0_1.xml');
-//        dd($xmlFiles);
-//        if (empty($xmlFiles)) {
-//            return new JsonResponse('empty file', 403);
-//        }
-//            foreach($xmlFiles as $file) {
-//
-//            $xml = Storage::get($file);
-//            $xmlArray = (new XmlService())->XmlToArray($xml);
-//        }
-//        dd($xmlArray);
-//
-//
-//        return view('home' );
     }
 }
 
